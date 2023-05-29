d58b901c19bf96136d419002fbad1949628be353
GeorgeKovshov <GeorgGreat@gmail.com>
2023-05-29 22:02:35 +0300

    made another isPalindrome function using stack

--- Source.cpp
+++ Source.cpp
@@ -1,124 +1,59 @@
 #include "Header.h"
+#include <stack>
 
-
-map<char, int> rome = {
-	   {'I', 1},
-	   {'V', 5},
-	   {'X', 10},
-	   {'L', 50},
-	   {'C', 100},
-	   {'D', 500},
-	   {'M', 1000}
-};
-
-
-
-
-int romanToInt(string s) {
-	int length = s.length();
-	int sum = 0;
-	int previous = 0;
-	for (int i = length - 1; i >= 0; i--) {
-		if (rome[s[i]] >= previous) {
-			sum += rome[s[i]];
+bool isPalindrome(ListNode* node) {
+	stack<int> my_stack;
+	ListNode* node_copy = node;
+	int length = 0;
+	while (node_copy) {
+		node_copy = node_copy->next;
+		length++;
+	}
+	bool result = true;
+	bool middle = (length % 2 != 0) ? true : false;
+	cout << "length is: " << length;
+	length = length / 2;
+	cout << ", half length is: " << length << endl;
+	while (node) {
+		cout << "cur. length: " << length << ", node: " << node->val << endl;
+		if (length > 0) {
+			my_stack.push(node->val);
+		}
+		else if (length < 0 and !my_stack.empty()) {
+			if (my_stack.top() != node->val) {
+				result = false;
+			}
+			my_stack.pop();	
+		}
+		else if (length == 0) {
+			if (!middle) {
+				my_stack.push(node->val);
+			}
+			
+			
 		}
 		else {
-			sum -= rome[s[i]];
+			result = false;
 		}
-		previous = rome[s[i]];
-
+		length--;
+		node = node->next;
 	}
 
-	return sum;
-}
-
-
-struct ListNode {
-	int val;
-	ListNode* next;
-	ListNode() : val(0), next(nullptr) {}
-	ListNode(int x) : val(x), next(nullptr) {}
-	ListNode(int x, ListNode* next) : val(x), next(next) {}
-
-
-	void add(int x) {
-		ListNode* n = this;
-		while (n->next) {
-			n = n->next;
-		}
-		n->next = new ListNode(x);
-	}
-
-	void show() {
-		ListNode* n = this;
-		while (n) {
-			cout << n->val << " ";
-			n = n->next;
-		}
-	}
-	
-};
-
-
-bool isPalindrome(ListNode* head) {
-	ListNode* head2 = head->next;
-	int count = 1;
-	int reverse_count = 100;
-	bool is_palindrome = false;
-	map<int, int> nums = { {0, head->val} };
-	while (head2) {
-		nums[count] = head2->val;
-
-		if (reverse_count < 0) {
-			is_palindrome = false;
-			reverse_count = 100;
-		}
-
-		if (nums[reverse_count] == nums[count] && is_palindrome == true) {
-			reverse_count -= 1;
-		}
-		else if (nums[reverse_count] != nums[count] && is_palindrome == true) {
-			is_palindrome = false;
-			reverse_count = 100;
-		}
-
-		if (nums[count - 1] == nums[count] && is_palindrome == false) {
-			is_palindrome = true;
-			reverse_count = count - 2;
-		}
-		head2 = head2->next;
-		count++;
-	}
-	
-	/*
-	for (int x = 0; x < count; x++) {
-		cout << nums[x] << " ";
-	}*/
-	
-	if (reverse_count == 0) {
-		return true;
-	}
-	return false;
+	return result;
 }
 
 
 
 int main(void) {
-	//romanToInt("MCMXCIV");
-	///cout << romanToInt("MDIX");
-	//cout << isPalindrome("MDIX");
-	ListNode node;
-	node.add(1);
+
+	ListNode node(1);
+	node.add(2);
 	node.add(2);
 	node.add(2);
 	node.add(1);
 
 
-;
-	node.show();
-
-
-	//cout << isPalindrome(&node);
+	cout << endl << isPalindrome(&node);
 
 	
 }
